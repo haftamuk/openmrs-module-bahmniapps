7f33ae065c4cfa4ae7a4234b2d1994a6c3fa641f
Haftamu Kebede <haftamu.k@gmail.com>
2023-09-25 12:42:01 +0300

    Ayder logo

--- ui/test/unit/common/domain/services/localeService.spec.js
+++ ui/test/unit/common/domain/services/localeService.spec.js
@@ -1,67 +1,66 @@
-'use strict';
+"use strict";
 
-describe('localeService', function () {
+describe("localeService", function () {
+  var localeService;
+  var _$http;
+  var localesList = "en, es, fr";
+  var defaultLocale = "en";
+  var loginText = "Ayder HIS LOGIN";
+  var localeLangs = "locale codes language";
 
-    var localeService;
-    var _$http;
-    var localesList = "en, es, fr";
-    var defaultLocale = "en";
-    var loginText = "BAHMNI EMR LOGIN";
-    var localeLangs = "locale codes language";
-
-    beforeEach(function(){
-        module('bahmni.common.domain');
-        module(function ($provide){
-            _$http = jasmine.createSpyObj('$http', ['get']);
-            $provide.value('$http', _$http);
-        });
-
-        inject(function (_localeService_) {
-            localeService = _localeService_;
-        });
+  beforeEach(function () {
+    module("bahmni.common.domain");
+    module(function ($provide) {
+      _$http = jasmine.createSpyObj("$http", ["get"]);
+      $provide.value("$http", _$http);
     });
 
-    it('should fetch allowed list of locales', function(done){
-        _$http.get.and.callFake(function(param) {
-            return specUtil.respondWith({"data": localesList});
-        });
+    inject(function (_localeService_) {
+      localeService = _localeService_;
+    });
+  });
 
-        localeService.allowedLocalesList().then(function (response) {
-            expect(response.data).toEqual(localesList);
-            done();
-        });
+  it("should fetch allowed list of locales", function (done) {
+    _$http.get.and.callFake(function (param) {
+      return specUtil.respondWith({ data: localesList });
     });
 
-    it('should fetch default locale', function(done){
-        _$http.get.and.callFake(function(param) {
-            return specUtil.respondWith({"data": defaultLocale});
-        });
+    localeService.allowedLocalesList().then(function (response) {
+      expect(response.data).toEqual(localesList);
+      done();
+    });
+  });
 
-        localeService.defaultLocale().then(function (response) {
-            expect(response.data).toEqual(defaultLocale);
-            done();
-        });
+  it("should fetch default locale", function (done) {
+    _$http.get.and.callFake(function (param) {
+      return specUtil.respondWith({ data: defaultLocale });
     });
 
-    it('should fetch default login page text', function(done){
-        _$http.get.and.callFake(function(param) {
-            return specUtil.respondWith({"data": loginText});
-        });
+    localeService.defaultLocale().then(function (response) {
+      expect(response.data).toEqual(defaultLocale);
+      done();
+    });
+  });
 
-        localeService.getLoginText().then(function (response) {
-            expect(response.data).toEqual(loginText);
-            done();
-        });
+  it("should fetch default login page text", function (done) {
+    _$http.get.and.callFake(function (param) {
+      return specUtil.respondWith({ data: loginText });
     });
 
-    it('should fetch default locale languages', function(done){
-        _$http.get.and.callFake(function(param) {
-            return specUtil.respondWith({"data": localeLangs});
-        });
-
-        localeService.getLoginText().then(function (response) {
-            expect(response.data).toEqual(localeLangs);
-            done();
-        });
+    localeService.getLoginText().then(function (response) {
+      expect(response.data).toEqual(loginText);
+      done();
     });
+  });
+
+  it("should fetch default locale languages", function (done) {
+    _$http.get.and.callFake(function (param) {
+      return specUtil.respondWith({ data: localeLangs });
+    });
+
+    localeService.getLoginText().then(function (response) {
+      expect(response.data).toEqual(localeLangs);
+      done();
+    });
+  });
 });
